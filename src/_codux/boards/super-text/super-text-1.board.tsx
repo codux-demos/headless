34b231cbb8761925973f9411ef4312a7433f5669
Omry Nachman <omry@wix.com>
2023-09-03 10:20:46 +0300

    removed unused components & boards

--- src/_codux/boards/super-text/super-text-1.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from "@wixc3/react-board";
-import { SuperText } from "../../../components/super-text/super-text";
-
-export default createBoard({
-    name: "SuperText 1",
-    Board: () => <SuperText />,
-    isSnippet: true,
-});
--- src/_codux/boards/super-text/super-text.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { SuperText } from '../../../components/super-text/super-text';
-
-export default createBoard({
-    name: 'SuperText',
-    Board: () => <SuperText />,
-    isSnippet: true,
-});
