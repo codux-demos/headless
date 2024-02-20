5a663b8cf8158fcdae407cec48a1e0e747aed2ba
Omry Nachman <omry@wix.com>
2024-02-20 15:25:15 +0200

    renamed boards

--- src/_codux/boards/shad-cn-accordion.board.tsx
+++ src/_codux/boards/shad-cn-accordion.board.tsx
@@ -2,7 +2,7 @@ import { createBoard } from "@wixc3/react-board";
 import { NavigationMenuDemo } from "../../components/shadcd/menu-example";
 
 export default createBoard({
-    name: "ShadCn-Accordion",
+    name: "6-ShadCn-Accordion",
     Board: () => <NavigationMenuDemo />,
     isSnippet: true,
     environmentProps: {
